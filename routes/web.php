2126f6a6199120d1eb3c10a23dae7cc08e90bfbb
Mesbah Emami <emamim612@gmail.com>
2021-07-29 19:49:50 +0430

    exception handler for route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('number1', function () {
+    return 'kir';
+});
+
+Route::get('number2', function () {
+    return 'kos';
+});
